0f0412f2ef0b39a8ff034ee9fb06711d31da2d38
jeffrey <jeffrey.nasson@gmail.com>
2019-03-25 13:43:57 -0600

    commit

---
+++ helloWorld/src/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Jeff!");
+
+        int myFirstNumber = (5+10) + (2*15);
+        int mySecondNumber = 12;
+        int myThirdNumber= myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+    }
+}
